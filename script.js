cf3dd519f475ef50a572c378ad0bc70bc6e3dc1a
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-11-14 17:03:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,43 +1,34 @@
 function convertToRoman(num) {
     // Mapping of Roman numerals to their corresponding values
     const romanNumerals = {
-        1000: 'M',
-        900: 'CM',
-        500: 'D',
-        400: 'CD',
-        100: 'C',
-        90: 'XC',
-        50: 'L',
-        40: 'XL',
-        10: 'X',
-        9: 'IX',
-        5: 'V',
-        4: 'IV',
-        1: 'I'
+    M: 1000,
+    CM:900,
+    D: 500,
+    CD:400,
+    C: 100,
+    XC:90,
+    L: 50,
+    XL:40,
+    X: 10,
+    IX:9,
+    V: 5,
+    IV:4,
+    I: 1
     };
+let result='';
+   for (let  key in romanNumerals) {
+        while (num >= romanNumerals[key]) {
 
-    let result = '';
 
-    // Loop through the mapping and build the Roman numeral string
-    for (let value in romanNumerals) {
-        while (num >= value) {
-            result += romanNumerals[value];
-            num -= value;
+		
+            result +=key;
+            num -= romanNumerals[key];
         }
     }
-
     return result;
-
 }
 const number=prompt("enter the number");
  alert(convertToRoman(number));
-
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
 
-console.log(convertToRoman(36));
-
-
-
-
-// do not edit below this line
 module.exports = convertToRoman
