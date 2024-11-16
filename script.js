0a9e464abd521931362c9a6a65693a0bdd936b6b
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-11-16 20:12:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -30,5 +30,13 @@ let result='';
 const number=prompt("enter the number");
  alert(convertToRoman(number));
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-console.log(convertToRoman(36));
+
+// console.log(convertToRoman(36));
+
+
+
+
+// do not edit below this line
 module.exports = convertToRoman
+
+
